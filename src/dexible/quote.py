d42821236b7f1327ae8d9024a79897d1c07a0989
defi-gene <defigene@gmail.com>
2021-08-31 23:46:30 +0200

    Support "fixedPrice" in quotes

--- src/dexible/quote.py
+++ src/dexible/quote.py
@@ -5,19 +5,20 @@ class QuoteWrapper:
         self.api_client = api_client
 
     async def get_quote(self, token_in, token_out, amount_in, slippage_percent,
-                        max_rounds=None, max_fixed_gas=None):
+                        max_rounds=None, max_fixed_gas=None, fixed_price=None):
         return await get_quote(api_client=self.api_client,
                                token_in=token_in,
                                token_out=token_out,
                                amount_in=amount_in,
                                slippage_percent=slippage_percent,
                                max_rounds=max_rounds,
-                               max_fixed_gas=max_fixed_gas)
+                               max_fixed_gas=max_fixed_gas,
+                               fixed_price=fixed_price)
 
 
 async def get_quote(api_client, token_in, token_out, amount_in,
                     slippage_percent, max_rounds=None, min_order_size=None,
-                    max_fixed_gas=None):
+                    max_fixed_gas=None, fixed_price=None):
     if min_order_size is None:
         min_order_size = amount_in * 30 // 100
     if max_rounds:
@@ -35,5 +36,7 @@ async def get_quote(api_client, token_in, token_out, amount_in,
 
     if max_fixed_gas:
         quote_body["maxFixedGas"] = max_fixed_gas
+    if fixed_price:
+        quote_body['fixedPrice'] = fixed_price
 
     return await api_client.post("quotes", data=quote_body)
